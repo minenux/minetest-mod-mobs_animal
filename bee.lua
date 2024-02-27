28103e0f832a2eaf4c09320b1b37f62d4392ea0d
SwissalpS <swissalps@noreply.codeberg.org>
2024-02-27 00:05:45 +0000

    honey blocks and beehives aren't ground content
    * closes https://codeberg.org/tenplus1/mobs_animal/pulls/1
    * closes https://github.com/pandorabox-io/pandorabox.io/issues/836
    * Many node definitions seem to have not set is_ground_content to false,
      means jumping a ship near mapblocks that haven't been generated yet,
      can lead to loss of ships or at least parts of it.
    * improved and related to commit 18a62b452fe6c70d0c039b2cf2a15dd0d740c41b

--- bee.lua
+++ bee.lua
@@ -91,6 +91,7 @@ minetest.register_node(":mobs:beehive", {
 	sunlight_propagates = true,
 	walkable = true,
 	groups = {oddly_breakable_by_hand = 3, flammable = 1, disable_suffocation = 1},
+	is_ground_content = false,
 	sounds = default and default.node_sound_defaults(),
 
 	on_construct = function(pos)
@@ -165,6 +166,7 @@ minetest.register_node(":mobs:honey_block", {
 	description = S("Honey Block"),
 	tiles = {"mobs_honey_block.png"},
 	groups = {snappy = 3, flammable = 2},
+	is_ground_content = false,
 	sounds = default and default.node_sound_dirt_defaults()
 })
 
